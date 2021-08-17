8d46202b7ca02cba2ff5936c9fddfe285d7ac8d9
Johnny Ferreira da Silva <joferres@everis.com>
2021-08-16 21:00:30 -0300

    Configurando cliente do axios

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -1,4 +1,5 @@
 import { createContext, ReactNode } from "react";
+import { api } from "../services/api";
 
 type SignInCredentials = {
     email: string;
@@ -19,7 +20,12 @@ export function AuthProvider({children}: AuthProviderProps) {
     const isAuthenticated = false;
 
     async function signIn({email, password}: SignInCredentials) {
-        console.log({email, password});
+        const response = await api.post('sessions', {
+            email,
+            password
+        })
+        console.log(response.data);
+        
     }
     return (
         <AuthContext.Provider value={{signIn, isAuthenticated}} >
