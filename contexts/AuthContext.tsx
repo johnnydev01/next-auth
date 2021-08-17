037bbef4939c782a51c1ff29fd41990feef3d42e
Johnny Ferreira da Silva <joferres@everis.com>
2021-08-16 22:09:14 -0300

    Recuperando estado de autenticação

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -1,7 +1,7 @@
-import { createContext, ReactNode, useState } from "react";
+import { createContext, ReactNode, useEffect, useState } from "react";
 import { api } from "../services/api";
 import Router from 'next/router';
-import { setCookie } from 'nookies';
+import { setCookie, parseCookies } from 'nookies';
 
 
 type SignInCredentials = {
@@ -29,6 +29,17 @@ export function AuthProvider({children}: AuthProviderProps) {
     const [user, setUser] = useState<User>();
     const isAuthenticated = !!user;
 
+    useEffect(() => {
+        const { 'nextauth.token': token} = parseCookies();
+
+        if(token) {
+            api.get('/me').then(response => {
+                const { email, permissions, roles } = response.data;
+
+                setUser({ email, permissions, roles});
+            })
+        }
+    }, [])
     async function signIn({email, password}: SignInCredentials) {
         try {
             const response = await api.post('sessions', {
@@ -54,6 +65,9 @@ export function AuthProvider({children}: AuthProviderProps) {
                 permissions,
                 roles
             });
+
+            api.defaults.headers['Authorization'] = `Bearer ${token}`
+
             Router.push('/dashboard')
         } catch (error) {
             console.log(error)
