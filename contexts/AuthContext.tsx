e7f4ad578eedc936ee95850a25ccb35e49cc21df
Johnny Ferreira da Silva <joferres@everis.com>
2021-08-19 22:44:35 -0300

    Broadcast de logout

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -9,7 +9,8 @@ type SignInCredentials = {
     password: string;
 }
 type AuthContextData = {
-    signIn(credentials: SignInCredentials): Promise<void>;
+    signIn: (credentials: SignInCredentials) => Promise<void>;
+    signOut: () => void;
     user: User | undefined;
     isAuthenticated: boolean;
 };
@@ -25,9 +26,15 @@ type User = {
 }
 export const AuthContext = createContext({} as AuthContextData);
 
+let authChannel: BroadcastChannel;
+
+
 export function signOut() {
     destroyCookie(undefined, 'nexthauth.token');
     destroyCookie(undefined, 'nexthauth.refreshToken');
+
+    authChannel.postMessage('signOut')
+    
     Router.push('/');
 }
 
@@ -35,6 +42,20 @@ export function AuthProvider({children}: AuthProviderProps) {
     const [user, setUser] = useState<User>();
     const isAuthenticated = !!user;
 
+    useEffect(() => {
+        authChannel = new BroadcastChannel('auth');
+
+        authChannel.onmessage = (message) => {
+            switch(message.data) {
+                case 'sigOut':
+                    signOut();
+                    break;
+                
+                default: 
+                    break;
+            }
+        }
+    }, [])
     useEffect(() => {
         const { 'nextauth.token': token} = parseCookies();
 
@@ -76,14 +97,16 @@ export function AuthProvider({children}: AuthProviderProps) {
 
             api.defaults.headers['Authorization'] = `Bearer ${token}`
 
-            Router.push('/dashboard')
+            Router.push('/dashboard');
+
+            authChannel.postMessage('signIn')
         } catch (error) {
             console.log(error)
         }
         
     }
     return (
-        <AuthContext.Provider value={{signIn, isAuthenticated, user}} >
+        <AuthContext.Provider value={{signIn, signOut, isAuthenticated, user}} >
             {children}
         </AuthContext.Provider>
     )
